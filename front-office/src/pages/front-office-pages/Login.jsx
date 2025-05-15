991d550501c541fd924b5b570345e9569fa8abd4
mcouliba <mcouliba985@gmail.com>
2025-05-15 12:44:12 +0000

    development finish

--- front-office/src/pages/front-office-pages/Login.jsx
+++ front-office/src/pages/front-office-pages/Login.jsx
@@ -16,7 +16,6 @@ function Login() {
             try {
                   const response = await fetch('http://localhost:8000/api/login', {
                         method: 'POST',
-                        headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify({ email, password }),
                   });
 
